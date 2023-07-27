385e29e5b1006c72287c55109c690aa50afada48
Alex Knoll <alex@agk.io>
2023-07-27 13:56:36 -0600

    populate index with lesson plans from Story Blok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
 import { defineConfig } from 'astro/config';
+import { loadEnv } from 'vite';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+import storyblok from '@storyblok/astro';
+
+const { PUBLIC_STORYBLOK_ACCESS_TOKEN } = loadEnv(import.meta.env.PUBLIC_STORYBLOK_ACCESS_TOKEN, process.cwd(), '');
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    storyblok({
+      accessToken: PUBLIC_STORYBLOK_ACCESS_TOKEN,
+      apiOptions: {
+        region: "us",
+      }
+    }),
+    tailwind(),
+  ],
 });
