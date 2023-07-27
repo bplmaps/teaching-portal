56ac4b713f2579d46dd59cf95519ead2b5621ab1
Alex Knoll <alex@agk.io>
2023-07-27 13:44:16 -0600

    add basic Tailwind layout skeleton

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,24 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				regal: {
+					50: '#eff8ff',
+					100: '#def1ff',
+					200: '#b5e4ff',
+					300: '#74d0ff',
+					400: '#2bbaff',
+					500: '#00a1f8',
+					600: '#007fd5',
+					700: '#0065ac',
+					800: '#00568e',
+					900: '#054571',
+					950: '#032d4e',
+				},
+				
+			}
+		},
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
